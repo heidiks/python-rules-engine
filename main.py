25cd9ae4bb18c91f99cb10f94beb166acbbe31ab
Heidi Kussakawa <heidiks@gmail.com>
2021-03-10 16:21:27 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, make_response
+from motor_calculo import FormulaRequest, calculate_formula
+
+app = Flask("rules-engine")
+
+@app.route('/formula', methods=['POST'])
+def formula():
+    body = request.get_json()
+    formula_request = FormulaRequest(body['variables'], body['formula'])
+
+    calculated = calculate_formula(formula_request)
+    response = make_response(calculated.toJSON(), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+app.run()
